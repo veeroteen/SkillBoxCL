303bcfedd40b63b831e84fb761fde53c9da28158
veeroteen <73069405+veeroteen@users.noreply.github.com>
2022-05-10 19:54:22 +0700

    mod18.2,3

--- Task23.cpp
+++ Task23.cpp
@@ -4,14 +4,20 @@
 #include "Tasks.h"
 using namespace std;
 
-int jump(int n,int k = 3)
-{
-    
+int rec (int n, int k = 3){
+int r = 0;
+    if(n == 0)
+    {
+
+        return 1;
+    }
+    for (int i = 1; i < k+1 && i < n+1; i++){
+        r +=rec(n - i, k);
+    }
+    return r;
 }
-
-
 
 void Task23(){
 
-
+cout << endl << rec(3,2) << endl;
 }
